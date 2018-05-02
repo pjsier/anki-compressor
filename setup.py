2bc3014b0ec119050c5fd757ead3734359a833e1
pjsier <pjsier@gmail.com>
2018-05-02 08:25:40 -0500

    Update packaging information

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
+import os
 from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='anki-compressor',
-    version='0.0.5',
+    version='0.0.6',
     url='https://github.com/pjsier/anki-compressor',
     license='MIT',
     author='Patrick Sier',
-    author_email='pjsier@gmail.com',
     description='Compress Anki deck .apkg file size',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         'Pillow',
         'pydub',
@@ -18,5 +26,14 @@ setup(
         'console_scripts': [
             'anki-compressor = anki_compressor.__main__:main'
         ]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/pjsier/anki-compressor/issues',
+        'Source': 'https://github.com/pjsier/anki-compressor',
     },
 )
