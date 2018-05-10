fe0b92a80ec76fc0ccb6707e3b52a5173e2accc6
pjsier <pjsier@gmail.com>
2018-05-10 17:17:26 -0500

    Update for markdown

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='anki-compressor',
-    version='0.0.6',
+    version='0.0.7',
     url='https://github.com/pjsier/anki-compressor',
     license='MIT',
     author='Patrick Sier',
