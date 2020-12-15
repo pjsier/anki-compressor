29a75b96d4d758365b0549b9cf67bd50b2b56749
pjsier <pjsier@gmail.com>
2020-12-14 19:55:49 -0600

    style: add linting

--- anki_compressor/__main__.py
+++ anki_compressor/__main__.py
@@ -1,44 +1,48 @@
-import os
-import sys
-import json
-import sqlite3
 import argparse
+import json
+import os
+import sqlite3
 from io import BytesIO
-from zipfile import ZipFile
 from tempfile import NamedTemporaryFile
-from pydub import AudioSegment
+from zipfile import ZipFile
+
 from PIL import Image
+from pydub import AudioSegment
 from tqdm import tqdm
 
-
-IMAGE_EXT = ('jpg', 'jpeg', 'png', 'tif', 'tiff', 'gif', 'webp')
-AUDIO_EXT = ('wav', 'mp3', 'ogg', 'flac', 'mp4', 'swf', 'mov', 'mpeg', 'mkv', 'm4a', '3gp', 'spx', 'oga')
-
-
-parser = argparse.ArgumentParser(description='Compress Anki .apkg file size')
-parser.add_argument('-i', '--input', dest='input', required=True,
-                    help='Input .apkg file to compress')
-parser.add_argument('-o', '--output', dest='output', required=False,
-                    help='Output file to write, defaults to MIN_<INPUT>')
-parser.add_argument('-q', '--quality', dest='quality', default=50, type=int,
-                    help='Quality value for image compression (0-100), defaults to 50')
-parser.add_argument('-b', '--bitrate', dest='bitrate', default='48k',
-                    help='ffmpeg-compliant bitrate value for audio compression, defaults to 48k')
+IMAGE_EXT = ("jpg", "jpeg", "png", "tif", "tiff", "gif", "webp")
+AUDIO_EXT = (
+    "wav",
+    "mp3",
+    "ogg",
+    "flac",
+    "mp4",
+    "swf",
+    "mov",
+    "mpeg",
+    "mkv",
+    "m4a",
+    "3gp",
+    "spx",
+    "oga",
+)
 
 
 def update_db(conn, cur, filename, ext):
-    new_filename = '.'.join(['.'.join(filename.split('.')[:-1]), ext])
+    new_filename = ".".join([".".join(filename.split(".")[:-1]), ext])
     cur.execute(
         "SELECT id, flds, sfld FROM notes WHERE flds LIKE ? OR sfld LIKE ?",
-        ('%{}%'.format(filename), '%{}%'.format(filename))
+        ("%{}%".format(filename), "%{}%".format(filename)),
     )
     rows = cur.fetchall()
     for row in rows:
         cur.execute(
-            'UPDATE notes SET flds = ?, sfld = ? WHERE id = ?',
-            (str(row[1]).replace(filename, new_filename),
-            str(row[2]).replace(filename, new_filename),
-            row[0])
+            "UPDATE notes SET flds = ?, sfld = ? WHERE id = ?",
+            (
+                str(row[1]).replace(filename, new_filename),
+                str(row[2]).replace(filename, new_filename),
+                row[0],
+            ),
         )
         conn.commit()
 
@@ -51,14 +55,14 @@ def compress_image(ext, image_bytes, quality=50):
     try:
         im = Image.open(img_buf)
         output_buf = BytesIO()
-        im.convert('RGB').save(output_buf, 'JPEG', optimize=True, quality=quality)
+        im.convert("RGB").save(output_buf, "JPEG", optimize=True, quality=quality)
 
         return output_buf.getvalue()
-    except:
-        return None
+    except Exception:
+        pass
 
 
-def compress_audio(ext, audio_bytes, bitrate='48k'):
+def compress_audio(ext, audio_bytes, bitrate="48k"):
     in_tmp = NamedTemporaryFile(delete=False)
     out_tmp = NamedTemporaryFile(delete=False)
 
@@ -69,77 +73,107 @@ def compress_audio(ext, audio_bytes, bitrate='48k'):
 
     try:
         segment = AudioSegment.from_file(in_tmp.name, ext)
-        segment.export(out_tmp.name, format='ogg', bitrate=bitrate)
+        segment.export(out_tmp.name, format="ogg", bitrate=bitrate)
 
-        with open(out_tmp.name, 'rb') as f:
+        with open(out_tmp.name, "rb") as f:
             compressed_audio = f.read()
 
         os.remove(in_tmp.name)
         os.remove(out_tmp.name)
 
         return compressed_audio
-    except:
-        return None
+    except Exception:
+        pass
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Compress Anki .apkg file size")
+    parser.add_argument(
+        "-i",
+        "--input",
+        dest="input",
+        required=True,
+        help="Input .apkg file to compress",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        dest="output",
+        required=False,
+        help="Output file to write, defaults to MIN_<INPUT>",
+    )
+    parser.add_argument(
+        "-q",
+        "--quality",
+        dest="quality",
+        default=50,
+        type=int,
+        help="Quality value for image compression (0-100), defaults to 50",
+    )
+    parser.add_argument(
+        "-b",
+        "--bitrate",
+        dest="bitrate",
+        default="48k",
+        help="ffmpeg-compliant bitrate value for audio compression, defaults to 48k",
+    )
+
     args = parser.parse_args()
 
     output_file = args.output
     if output_file is None:
         output_file = os.path.join(
-            os.path.dirname(args.input),
-            'MIN_{}'.format(os.path.basename(args.input))
+            os.path.dirname(args.input), "MIN_{}".format(os.path.basename(args.input))
         )
 
     if args.input == output_file:
-        raise ValueError('Output file cannot have the same name as input')
-    
+        raise ValueError("Output file cannot have the same name as input")
+
     anki_zip = ZipFile(args.input)
-    if not 'media' in anki_zip.namelist():
-        raise ValueError('{} does not contain a media file'.format(args.input))
-    
+    if "media" not in anki_zip.namelist():
+        raise ValueError("{} does not contain a media file".format(args.input))
+
     # Create new zip, temp file for SQLite database
-    compressed_zip = ZipFile(output_file, 'w')
+    compressed_zip = ZipFile(output_file, "w")
     db_tmp = NamedTemporaryFile(delete=False)
-    db_tmp.write(anki_zip.read('collection.anki2'))
+    db_tmp.write(anki_zip.read("collection.anki2"))
     db_tmp.seek(0)
     db_tmp.close()
     conn = sqlite3.connect(db_tmp.name)
     cur = conn.cursor()
 
     # Read media JSON, create new dict for updates
-    media_json = json.loads(anki_zip.read('media').decode('utf-8'))
+    media_json = json.loads(anki_zip.read("media").decode("utf-8"))
     media = {}
 
     for k, v in tqdm(media_json.items()):
-        if len(v.split('.')) < 2:
+        if len(v.split(".")) < 2:
             compressed_zip.writestr(k, anki_zip.read(k))
             continue
-        ext = v.split('.')[-1].lower()
+        ext = v.split(".")[-1].lower()
         contents = None
         if ext in IMAGE_EXT:
             contents = compress_image(ext, anki_zip.read(k), quality=args.quality)
             if contents is not None:
-                update_db(conn, cur, v, 'jpg')
-                v = '.'.join(['.'.join(v.split('.')[:-1]), 'jpg'])
+                update_db(conn, cur, v, "jpg")
+                v = ".".join([".".join(v.split(".")[:-1]), "jpg"])
         elif ext in AUDIO_EXT:
             contents = compress_audio(ext, anki_zip.read(k), bitrate=args.bitrate)
             if contents is not None:
-                update_db(conn, cur, v, 'ogg')
-                v = '.'.join(['.'.join(v.split('.')[:-1]), 'ogg'])
+                update_db(conn, cur, v, "ogg")
+                v = ".".join([".".join(v.split(".")[:-1]), "ogg"])
         if contents is None:
             contents = anki_zip.read(k)
         media[k] = v
         compressed_zip.writestr(k, contents)
 
-    compressed_zip.writestr('media', json.dumps(media))
+    compressed_zip.writestr("media", json.dumps(media))
     conn.close()
-    with open(db_tmp.name, 'rb') as db_file:
-        compressed_zip.writestr('collection.anki2', db_file.read())
+    with open(db_tmp.name, "rb") as db_file:
+        compressed_zip.writestr("collection.anki2", db_file.read())
     os.remove(db_file.name)
     compressed_zip.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -1,39 +1,34 @@
 import os
+
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 
 setup(
-    name='anki-compressor',
-    version='0.0.7',
-    url='https://github.com/pjsier/anki-compressor',
-    license='MIT',
-    author='Patrick Sier',
-    description='Compress Anki deck .apkg file size',
+    name="anki-compressor",
+    version="0.0.7",
+    url="https://github.com/pjsier/anki-compressor",
+    license="MIT",
+    author="Patrick Sier",
+    description="Compress Anki deck .apkg file size",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    install_requires=[
-        'Pillow',
-        'pydub',
-        'tqdm'
-    ],
-    packages=['anki_compressor'],
+    long_description_content_type="text/markdown",
+    install_requires=["Pillow", "pydub", "tqdm"],
+    packages=["anki_compressor"],
     entry_points={
-        'console_scripts': [
-            'anki-compressor = anki_compressor.__main__:main'
-        ]
+        "console_scripts": ["anki-compressor = anki_compressor.__main__:main"]
     },
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
     ],
     project_urls={
-        'Bug Reports': 'https://github.com/pjsier/anki-compressor/issues',
-        'Source': 'https://github.com/pjsier/anki-compressor',
+        "Bug Reports": "https://github.com/pjsier/anki-compressor/issues",
+        "Source": "https://github.com/pjsier/anki-compressor",
     },
 )
